674a2c5f47f28adf21e4c0b9511511323d11245e
Timothe LEBRIZE <tlebrize@e3r8p21.42.fr>
2018-01-23 17:04:28 +0100

    generation !

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -4,10 +4,20 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour {
 
-	CarScript					car;
+	CarScript			car;
+	Camera				camera;
 	
+	[HideInInspector]
+	public GameObject 	currentTile;
+
+	void OnTriggerEnter2d(Collider2D collider) {
+		if (collider.gameObject.tag == "road")
+			currentTile = collider.gameObject;
+	}
+
 	void Start() {
 		car = gameObject.GetComponent<CarScript>();
+		camera = Camera.main;
 	}
 
 	void Update () {
@@ -21,5 +31,6 @@ public class PlayerScript : MonoBehaviour {
 			car.inputQueue.Enqueue(CarScript.Behaviours.Left);
 		if (horizontal < 0)
 			car.inputQueue.Enqueue(CarScript.Behaviours.Right);		
+		camera.transform.position = new Vector3(transform.position.x, transform.position.y, -10f);
 	}
 }
---
+++ Assets/Scripts/RoadManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Linq;	
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadManager : MonoBehaviour {
+
+	public Transform	player;
+	
+	GameObject			currentTile;
+	List<Transform>		road;
+
+	void Build() {
+		Debug.Log("Build");
+	}
+
+	float DistToPlayer(Transform tile) {
+		return Vector3.Distance(tile.position, player.position);
+	}
+
+	void UpdateRoad() {
+		int destroyed = 0;
+		while (road.Count > 0 && DistToPlayer(road[0]) > 6f) {
+			Destroy(road[0].gameObject);
+			road.RemoveAt(0);
+			destroyed++;
+		}
+		while (destroyed-- > 0)
+			Build();
+	}
+
+	void Start () {
+		List<GameObject> roadGO = new List<GameObject>(GameObject.FindGameObjectsWithTag("road"));
+		road = new List<Transform>();
+		foreach (GameObject tile in roadGO) {
+			road.Add(tile.transform);
+		}
+		InvokeRepeating("UpdateRoad", 0f, 1f);
+	}
+}
