102633c73c326bd305ec74252f845ca1bf84a932
Timothe LEBRIZE <tlebrize@e3r8p21.42.fr>
2018-01-23 16:00:50 +0100

    base

---
+++ Assets/Scripts/CarScript.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarScript : MonoBehaviour {
+
+	private Rigidbody2D			rb2d;
+	private float				steeringDirection;
+	
+	[HideInInspector]
+	public Queue<Behaviours>	inputQueue;
+	[HideInInspector]
+	public enum					Behaviours {Accelerate, Brake, Left, Right};
+
+	public float 				accelerationSpeed;
+	public float 				brakingSpeed;
+	public float				steering;	
+
+	void Start() {
+		inputQueue = new Queue<Behaviours>();
+		rb2d = gameObject.GetComponent<Rigidbody2D>();
+	}
+	
+	void Accelerate() {
+		Vector3 velocity = transform.up * accelerationSpeed / 60f;
+		rb2d.AddForce(velocity, ForceMode2D.Impulse);
+	}
+
+	void Brake() {
+		Vector3 velocity = -transform.up * brakingSpeed / 60f;
+		rb2d.AddForce(velocity, ForceMode2D.Impulse);
+	}
+
+	void Steer() {
+		rb2d.rotation += steeringDirection * steering * (rb2d.velocity.magnitude / 5.0f);
+	}
+
+	void ApplyInput(Behaviours behaviour) {
+		if (behaviour == Behaviours.Accelerate)
+			Accelerate();
+		else if (behaviour == Behaviours.Brake)
+			Brake();
+		else if (behaviour == Behaviours.Left) {
+			steeringDirection = -1;
+			Steer();
+		} else if (behaviour == Behaviours.Right) {
+			steeringDirection = 1;
+			Steer();
+		}
+	}
+
+	void FixedUpdate() {
+		while (inputQueue.Count > 0)
+			ApplyInput(inputQueue.Dequeue());
+	}
+}
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+
+	CarScript					car;
+	
+	void Start() {
+		car = gameObject.GetComponent<CarScript>();
+	}
+
+	void Update () {
+		float vertical = Input.GetAxis("Vertical");
+		float horizontal = Input.GetAxis("Horizontal");
+		if (vertical > 0)
+			car.inputQueue.Enqueue(CarScript.Behaviours.Accelerate);
+		if (vertical < 0)
+			car.inputQueue.Enqueue(CarScript.Behaviours.Brake);
+		if (horizontal > 0)
+			car.inputQueue.Enqueue(CarScript.Behaviours.Left);
+		if (horizontal < 0)
+			car.inputQueue.Enqueue(CarScript.Behaviours.Right);		
+	}
+}
